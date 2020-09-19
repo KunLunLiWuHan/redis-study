f2c0762913e7ba334f425930644a68a5ba283b6f
小伦 <43144822+KunLunLiWuHan@users.noreply.github.com>
2020-09-19 17:02:01 +0800

    Update TestPing.java

--- src/com/xiaolun/redis/TestPing.java
+++ src/com/xiaolun/redis/TestPing.java
@@ -4,7 +4,7 @@ import redis.clients.jedis.Jedis;
 
 public class TestPing {
 	private static final String hostAddress = "192.168.10.101"; //远程
-//	private static final String hostAddress = "127.0.0.1"; // 本地
+//	private static final String hostAddress = "127.0.0.1";      // 本地
 
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis(hostAddress, 6379);
